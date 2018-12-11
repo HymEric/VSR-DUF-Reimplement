566db2e1e887b50269c95886bef7040b17c4b5b2
HymEric <951523291@qq.com>
2018-12-11 19:15:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+## Introdction
+It is a re-implementation of paper named "Deep Video Super-Resolution Network Using Dynamic Upsampling Filters Without Explicit Motion Compensation" which I would say VSR-DUF model. There are both training codes and test codes about VSR-DUF based tensorflow.  
+
+If you are interested in Image Super-rerolution(ISR) or Video Super-resolution(VSR), you maybe know there is paper author github: [https://github.com/yhjo09/VSR-DUF/](https://github.com/yhjo09/VSR-DUF/ "VSR-DUF")  about VSR-DUF, but it's only can be tested with your Low-resolution frames and can't be trained. Therefor, I try to re-implement this paper based above github repository. I hope this will be helpful for you. 
+
+If you think it is useful, please star it. Thank you.
+
+## Environments
+TensorFlow:1.8.0  
+pillow:5.3.0  
+numpy:1.15.4
+
+## Data-preparation
+
+### The directory tree
+Low-resolution:  
+**./data/x_train_data4x/:** The scaled frames by 4x which will be used in train stage  
+**./data/x_valid_data4x/:** The scaled frames by 4x which will be used in valid stage  
+**./data/x_test_data4x/:** The scaled frames by 4x which will be used in test stage
+
+Original-resolution:  
+**./data/y_train_data/:** The HR frames which will be used in train stage coresponde to **./data/x_train_data4x/**  
+**./data/y_valid_data/:** The HR frames which will be used in valid stage coresponde to **./data/x_valid_data4x/**
+
+Results of output:  
+**./result_test/:** The output frames after VSR-DUF processing
+
+## Attention
+
+Before run train and test, your should prepare your datasets and check your environments if you want to run it successfully.   
+Please, remind that the performence will be determined by your selected training datasets. Good training datasets good performence!   
+Recommend: try to use public video dataset like [http://toflow.csail.mit.edu](http://toflow.csail.mit.edu), even though the performance may not be exactly the same as in the paper
+
+And when you are runing mytest.py, you maybe need to modify it as you need to choose a trined .pb model to test.
+
+## Run
+**Training: python mytrain.py  
+Testing: python mytest.py**
+
+If you want to improve it or change something acording your requirements, just feel free to modify it!
+
+## Author
+EricHym (Yongming He)   
+Interests: CV and Deep Learning  
+If you have or find any problems, this is my email: [yongminghe_eric@qq.com](yongminghe_eric@qq.com). And I'm glad to reply it.
+Thanks. 
---
+++ utils.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+def LoadImage(path, color_mode='RGB', channel_mean=None, modcrop=[0,0,0,0]):
+    '''Load an image using PIL and convert it into specified color space,
+    and return it as an numpy array.
+
+    https://github.com/fchollet/keras/blob/master/keras/preprocessing/image.py
+    The code is modified from Keras.preprocessing.image.load_img, img_to_array.
+    '''
+    ## Load image
+    img = Image.open(path)
+    if color_mode == 'RGB':
+        cimg = img.convert('RGB')
+        x = np.asarray(cimg, dtype='float32')
+
+    elif color_mode == 'YCbCr' or color_mode == 'Y':
+        cimg = img.convert('YCbCr')
+        x = np.asarray(cimg, dtype='float32')
+        if color_mode == 'Y':
+            x = x[:,:,0:1]
+
+    ## To 0-1
+    x *= 1.0/255.0
+
+    if channel_mean:
+        x[:,:,0] -= channel_mean[0]
+        x[:,:,1] -= channel_mean[1]
+        x[:,:,2] -= channel_mean[2]
+
+    if modcrop[0]*modcrop[1]*modcrop[2]*modcrop[3]:
+        x = x[modcrop[0]:-modcrop[1], modcrop[2]:-modcrop[3], :]
+
+    return x
+
+
+he_normal_init = tf.contrib.layers.variance_scaling_initializer(factor=2.0, mode='FAN_IN', uniform=False)
+
+def BatchNorm(input, is_train, decay=0.999, name='BatchNorm'):
+    '''
+    https://github.com/zsdonghao/tensorlayer/blob/master/tensorlayer/layers.py
+    https://github.com/ry/tensorflow-resnet/blob/master/resnet.py
+    http://stackoverflow.com/questions/38312668/how-does-one-do-inference-with-batch-normalization-with-tensor-flow
+    '''
+    from tensorflow.python.training import moving_averages
+    from tensorflow.python.ops import control_flow_ops
+    
+    axis = list(range(len(input.get_shape()) - 1))
+    fdim = input.get_shape()[-1:]
+    
+    with tf.variable_scope(name):
+        beta = tf.get_variable('beta', fdim, initializer=tf.constant_initializer(value=0.0))
+        gamma = tf.get_variable('gamma', fdim, initializer=tf.constant_initializer(value=1.0))
+        moving_mean = tf.get_variable('moving_mean', fdim, initializer=tf.constant_initializer(value=0.0), trainable=False)
+        moving_variance = tf.get_variable('moving_variance', fdim, initializer=tf.constant_initializer(value=0.0), trainable=False)
+  
+        def mean_var_with_update():
+            batch_mean, batch_variance = tf.nn.moments(input, axis)
+            update_moving_mean = moving_averages.assign_moving_average(moving_mean, batch_mean, decay, zero_debias=True)
+            update_moving_variance = moving_averages.assign_moving_average(moving_variance, batch_variance, decay, zero_debias=True)
+            with tf.control_dependencies([update_moving_mean, update_moving_variance]):
+                return tf.identity(batch_mean), tf.identity(batch_variance)
+
+        mean, variance = control_flow_ops.cond(is_train, mean_var_with_update, lambda: (moving_mean, moving_variance))
+
+    return tf.nn.batch_normalization(input, mean, variance, beta, gamma, 1e-3) #, tf.stack([mean[0], variance[0], beta[0], gamma[0]])
+
+def Conv3D(input, kernel_shape, strides, padding, name='Conv3d', W_initializer=he_normal_init, bias=True):
+    with tf.variable_scope(name):
+        W = tf.get_variable("W", kernel_shape, initializer=W_initializer)
+        if bias is True:
+            b = tf.get_variable("b", (kernel_shape[-1]),initializer=tf.constant_initializer(value=0.0))
+        else:
+            b = 0
+        
+    return tf.nn.conv3d(input, W, strides, padding) + b
+
+def depth_to_space_3D(x, block_size):
+    ds_x = tf.shape(x)
+    x = tf.reshape(x, [ds_x[0]*ds_x[1], ds_x[2], ds_x[3], ds_x[4]])
+    
+    y = tf.depth_to_space(x, block_size)
+    
+    ds_y = tf.shape(y)
+    x = tf.reshape(y, [ds_x[0], ds_x[1], ds_y[1], ds_y[2], ds_y[3]])
+    return x
+    
+def DynFilter3D(x, F, filter_size):
+    '''
+    3D Dynamic filtering
+    input x: (b, t, h, w)
+          F: (b, h, w, tower_depth, output_depth)
+          filter_shape (ft, fh, fw)
+    '''
+    # make tower
+    filter_localexpand_np = np.reshape(np.eye(np.prod(filter_size), np.prod(filter_size)), (filter_size[1], filter_size[2], filter_size[0], np.prod(filter_size)))
+    filter_localexpand = tf.Variable(filter_localexpand_np, trainable=False, dtype='float32',name='filter_localexpand') 
+    x = tf.transpose(x, perm=[0,2,3,1])
+    x_localexpand = tf.nn.conv2d(x, filter_localexpand, [1,1,1,1], 'SAME') # b, h, w, 1*5*5
+    x_localexpand = tf.expand_dims(x_localexpand, axis=3)  # b, h, w, 1, 1*5*5
+    x = tf.matmul(x_localexpand, F) # b, h, w, 1, R*R
+    x = tf.squeeze(x, axis=3) # b, h, w, R*R
+
+    return x
+    
+def Huber(y_true, y_pred, delta, axis=None):
+    abs_error = tf.abs(y_pred - y_true)
+    quadratic = tf.minimum(abs_error, delta)
+    # The following expression is the same in value as
+    # tf.maximum(abs_error - delta, 0), but importantly the gradient for the
+    # expression when abs_error == delta is 0 (for tf.maximum it would be 1).
+    # This is necessary to avoid doubling the gradient, since there is already a
+    # nonzero contribution to the gradient from the quadratic term.
+    linear = (abs_error - quadratic)
+    losses = 0.5 * quadratic**2 + delta * linear
+    return tf.reduce_mean(losses, axis=axis)
